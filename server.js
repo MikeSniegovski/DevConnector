e33aa077e839ac71603df2feed20288d3f16c0b3
MikeSniegovski <michal.sniegowski@protonmail.ch>
2020-03-22 16:51:53 +0100

    Added routes and user registration.

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+//conenct DB
+
+connectDB();
+
+//Init Middlewere
+
+app.use(express.json({extended: false}))
 
 app.get('/', (req, res) => res.send('running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
